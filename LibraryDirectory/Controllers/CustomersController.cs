aed0bef4842768bb476c9e553e69140f8c80830c
Aleksander Niedźwiedź <aleksandern1116@gmail.com>
2017-07-04 02:15:15 +0200

    Added basic customer model

---
+++ LibraryDirectory/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using LibraryDirectory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryDirectory.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private CustomerDbContext db = new CustomerDbContext();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(db.Customers.ToList());
+        }
+    }
+}
---
+++ LibraryDirectory/Models/CustomerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LibraryDirectory.Models
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public int Phone { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class CustomerDbContext : DbContext
+    {
+        public DbSet<CustomerModel> Customers { get; set; }
+    }
+}
